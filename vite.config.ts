a123250b3a6ac866b0b59d8d32819093f1156b1d
eeluwole1 <engrwolesax@gmail.com>
2025-10-23 10:18:19 -0500

    Created Portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mathew-portfolio/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+});
